b22e0e793c05d3e16c32b32ee84d1df00ba3e24c
fluxrider <doompilot@gmail.com>
2020-04-14 19:12:44 -0400

    cairo x11

--- cairo_x11/dude.c
+++ cairo_x11/dude.c
@@ -5,6 +5,62 @@
 #include <stdlib.h>
 #include <cairo-xlib.h>
 #include <X11/Xlib.h>
+#include <time.h>
+#include <inttypes.h>
+#include <math.h>
+
+void draw_eye(cairo_t * g, int x, int y, int r, double lid) {
+  // eye ball
+  cairo_set_source_rgb(g, .5, .5, .5);
+  cairo_arc(g, x, y, r, 0, M_PI * 2);
+  cairo_fill(g);
+  // eye pupil
+  int rp1 = r / 5;
+  int rp2 = r / 7;
+  cairo_set_source_rgb(g, .5, 0, 0);
+  cairo_arc(g, x, y, rp1, 0, M_PI * 2);
+  cairo_fill(g);
+  cairo_set_source_rgb(g, 0, 0, 0);
+  cairo_arc(g, x, y, rp2, 0, M_PI * 2);
+  cairo_fill(g);
+  // eye lid
+  int h = (int)(2*r * lid);
+  cairo_save(g);
+  cairo_rectangle(g, x - r, y - r, r * 2, h);
+  cairo_clip(g);
+  cairo_set_source_rgb(g, 1, .89, .82);
+  cairo_arc(g, x, y, r, 0, M_PI * 2);
+  cairo_fill(g);
+  cairo_restore(g);
+  // eye outline
+  cairo_set_source_rgb(g, 0, 0, 0);
+  cairo_arc(g, x, y, r, 0, M_PI * 2);
+  // eye lid outline
+  int h2 = fabs(r-h);
+  int rl = (int)(sqrt(r*r - h2*h2));
+  cairo_move_to(g, x - rl, y - r + h);
+  cairo_line_to(g, x + rl, y - r + h);
+  cairo_close_path(g);
+  // eye lashes?
+  // commit outlines
+  cairo_stroke(g);
+}
+
+uint64_t currentTimeMillis() {
+  struct timespec tp;
+  if(clock_gettime(CLOCK_MONOTONIC, &tp) == -1) { perror("read"); exit(1); }
+  return tp.tv_sec * 1000 + tp.tv_nsec / 1000000;
+}
+
+void random_init() {
+  srand(time(NULL));
+}
+
+int random_inclusive(int low, int high) {
+  if(low == high) return low;
+  if(low > high) { printf("Error in random_inclusive. Low > High. %d %d\n", low, high); return low; }
+  return rand() % (high - low + 1) + low;
+}
 
 //This function should give us a new x11 surface to draw on.
 cairo_surface_t* create_x11_surface(Display *d, int x, int y)
@@ -25,6 +81,10 @@ cairo_surface_t* create_x11_surface(Display *d, int x, int y)
 
 int main(int argc, char** argv)
 {
+    random_init();
+    int W = 800;
+    int H = 450;
+
     Display *d = XOpenDisplay(NULL);
     if (d == NULL) {
         fprintf(stderr, "Failed to open display\n");
@@ -32,49 +92,49 @@ int main(int argc, char** argv)
     }
     //create a new cairo surface in an x11 window as well as a cairo_t* to draw
     //on the x11 window with.
-    cairo_surface_t* surface = create_x11_surface(d, 300, 200);
-    cairo_t* cr = cairo_create(surface);
-    char *text = argv[1];
-    size_t text_len = 0;
+    cairo_surface_t* surface = create_x11_surface(d, W, H);
+    cairo_t* g = cairo_create(surface);
 
-    if (argc != 2)
-        text = NULL;
-    else
-        text_len = strlen(text);
-
+    int frame_count = 0;
+    uint64_t t0 = currentTimeMillis();
     while(1)
     {
-        // Clear the background
-        cairo_set_source_rgb(cr, 0, 0, 0);
-        cairo_paint(cr);
+        // time flow
+        uint64_t t1 = currentTimeMillis();
+        double delta_time = (t1 - t0) / 1000.0;
+        if (delta_time < (1.0 / 60)) continue; // commented out, don't cap
+        t0 = t1;
+        // fps
+        frame_count++;
+        printf("%d: %f\n", frame_count, 1 / delta_time);
 
-        //draw some text
-        cairo_select_font_face(cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-        cairo_set_font_size(cr, 32.0);
-        cairo_set_source_rgb(cr, 0, 0, 1.0);
-        cairo_move_to(cr, 10.0, 25.0);
+        // cairo clear
+        cairo_set_source_rgb(g, 1, 1, 1);
+        cairo_paint(g);
+        
+        // eyes (crazy random bg)
+        for(int i = 0; i < 1000; i++) {
+          int r = random_inclusive(0, fmin(H, W) / 5);
+          int x = random_inclusive(0, W + 2*r) - r;
+          int y = random_inclusive(0, H + 2*r) - r;
+          double lid = random_inclusive(0, 1000) / 1000.0;
+          draw_eye(g, x, y, r, lid);
+        }
 
-        if (text)
-            cairo_show_text(cr, text);
-        else
-            cairo_show_text(cr, "usage: ./p1 <string>");
+        // eye (animated, centered)
+        double lid = t1 % 1000 / 1000.0;
+        if(lid < .5) {
+          lid *= 2;
+        } else {
+          lid = 1 - (lid - .5) * 2;
+        }
+        draw_eye(g, W / 2, H / 2, fmin(H, W) / 5, lid);
 
+
+        // flush
         cairo_surface_flush(surface);
         XFlush(d);
 
-        //pause for a little bit.
-        int c = getchar();
-
-        //change the text around so we can see the screen update.
-        memmove(text, &text[1], text_len);
-        if (text_len > 0)
-            text_len--;
-
-        printf("got char %c\n", c);
-        if(c == 'q')
-        {
-            break;
-        }
     }
 
     // XXX: Lots of other stuff isn't properly destroyed here
