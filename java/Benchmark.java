0fff22746fcc0c42c3012b7b5cebefd63671f313
fluxrider <doompilot@gmail.com>
2020-04-13 20:26:55 -0400

    function for drawing eye

--- java/Benchmark.java
+++ java/Benchmark.java
@@ -4,6 +4,35 @@ import java.awt.event.*;
 
 public class Benchmark {
 
+  public static void draw_eye(Graphics g, int x, int y, int r, double lid) {
+    // eye ball
+    g.setColor(Color.GRAY);
+    g.fillOval(x - r, y - r, r * 2, r * 2);
+    // eye pupil
+    int rp1 = r / 5;
+    int rp2 = r / 7;
+    //g.setColor(new Color(0x654321));
+    g.setColor(Color.RED);
+    g.fillOval(x - rp1, y - rp1, rp1 * 2, rp1 * 2);
+    g.setColor(Color.BLACK);
+    g.fillOval(x - rp2, y - rp2, rp2 * 2, rp2 * 2);
+    // eye lid
+    int h = (int)(2*r * lid);
+    Graphics2D clip = (Graphics2D)g.create();
+    clip.clip(new java.awt.geom.Rectangle2D.Double(x - r, y - r, r * 2, h));
+    clip.setColor(Color.PINK);
+    clip.fillOval(x - r, y - r, r * 2, r * 2);
+    clip.dispose();
+    // eye outline
+    g.setColor(Color.BLACK);
+    g.drawOval(x - r, y - r, r * 2, r * 2);
+    // eye lid outline
+    int h2 = Math.abs(r-h);
+    int rl = (int)(Math.sqrt(r*r - h2*h2));
+    g.drawLine(x - rl, y - r + h, x + rl, y - r + h);
+    // eye lashes?
+  }
+
   public static void main(String[] args) {
     // create window
     int W = 800;
@@ -26,42 +55,14 @@ public class Benchmark {
       System.out.println(frame_count + ": " + 1 / delta_time);
 
       // eye
-      int x = W / 2;
-      int y = H / 2;
-      int r = Math.min(H, W) / 5;
-      // eye ball
-      g.setColor(Color.GRAY);
-      g.fillOval((int) (x - r), (int) (y - r), (int) (r * 2), (int) (r * 2));
-      // eye pupil
-      int rp1 = r / 5;
-      int rp2 = r / 7;
-      //g.setColor(new Color(0x654321));
-      g.setColor(Color.RED);
-      g.fillOval((int) (x - rp1), (int) (y - rp1), (int) (rp1 * 2), (int) (rp1 * 2));
-      g.setColor(Color.BLACK);
-      g.fillOval((int) (x - rp2), (int) (y - rp2), (int) (rp2 * 2), (int) (rp2 * 2));
-      // eye lid
-      double eye_opening = t1 % 1000 / 1000.0;
-      if(eye_opening < .5) {
-        eye_opening *= 2;
+      double lid = t1 % 1000 / 1000.0;
+      if(lid < .5) {
+        lid *= 2;
       } else {
-        eye_opening = 1 - (eye_opening - .5) * 2;
+        lid = 1 - (lid - .5) * 2;
       }
-      int h = (int)(2*r * eye_opening);
-      Graphics2D clip = (Graphics2D)g.create();
-      clip.clip(new java.awt.geom.Rectangle2D.Double(x - r, y - r, r * 2, h));
-      clip.setColor(Color.PINK);
-      clip.fillOval((int) (x - r), (int) (y - r), (int) (r * 2), (int) (r * 2));
-      clip.dispose();
-      // eye outline
-      g.setColor(Color.BLACK);
-      g.drawOval((int) (x - r), (int) (y - r), (int) (r * 2), (int) (r * 2));
-      // eye lid outline
-      int h2 = Math.abs(r-h);
-      int rl = (int)(Math.sqrt(r*r - h2*h2));
-      g.drawLine(x - rl, y - r + h, x + rl, y - r + h);
-      // eye lashes?
-
+      draw_eye(g, W / 2, H / 2, Math.min(H, W) / 5, lid);
+ 
       // flush our drawing to the screen
       David.flushCanvas(canvas);
     }
